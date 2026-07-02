c551953486155815d7233186e4f038e1fe749393
ArmorialOfficiel <84501281+ArmorialOfficiel@users.noreply.github.com>
2026-07-02 13:06:56 +0200

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,15 +1,5 @@
-const calcul = require('./app');
+const { calcul } = require('./app');
 
-describe('Tests de la fonction calcul pour le Quality Gate', () => {
-
-  // 1. Test pour passer dans le "else" (a est inférieur ou égal à 10)
-  test('devrait additionner quand a est inférieur ou égal à 10', () => {
-    expect(calcul(1, 2)).toBe(3);
-  });
-
-  // 2. Test pour passer dans le "if" (a est strictement supérieur à 10)
-  test('devrait additionner quand a est supérieur à 10', () => {
-    expect(calcul(15, 5)).toBe(20);
-  });
-
+test('2 + 3 = 5', () => {
+  expect(calcul(2, 3)).toBe(5);
 });
